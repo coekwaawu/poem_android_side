9d89f5a34a9b65b5beec3d7bcf959d9b73358083
coekwaawu <121921479@qq.com>
2018-06-12 23:56:37 +0800

    get author,dynasty,content
    At home

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -7,16 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import javax.xml.transform.Result;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
+    private TextView title;
+    private TextView author_dynasty;
+    private TextView content;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -24,49 +23,45 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    initTextView("73add8822103");
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    initTextView("6d2dcbe80f96");
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    initTextView("2d6b0c83a500");
                     return true;
             }
             return false;
         }
     };
-/*
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextMessage = findViewById(R.id.message);
+        title = findViewById(R.id.title);
+        author_dynasty = findViewById(R.id.author_dynasty);
+        content = findViewById(R.id.content);
+        String poem_id = "632c5beb84eb";
+        initTextView(poem_id);
+
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-*/
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mTextMessage = findViewById(R.id.message);
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        String poem_id = "ee16df5673bc";
+    public void initTextView(String poem_id){
         PoemREST.PoemService service = ServiceGenerator.createService(PoemREST.PoemService.class);
         Call<Poem> call = service.getPoem(poem_id);
         Call<Poem> clone = call.clone();
         clone.enqueue(new Callback<Poem>() {
             @Override
             public void onResponse(Call<Poem> call, Response<Poem> response) {
-                mTextMessage.setText(response.body().title);
+                title.setText(response.body().title);
+                author_dynasty.setText(response.body().author+"  "+response.body().dynasty);
+                content.setText(response.body().content);
             }
-
             @Override
             public void onFailure(Call<Poem> call, Throwable t) {
-                mTextMessage.setText(t.getMessage());
             }
         });
     }
